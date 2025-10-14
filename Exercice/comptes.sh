4a0e8c8ae143104baf6a04696bf158939504c5dd
Lucy-Lou <lucy.lou.agard@gmail.com>
2025-10-14 10:41:11 +0200

    correction script

--- Exercice/comptes.sh
+++ Exercice/comptes.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 echo "Nombre de location en 2016 :"
-cat 2016/* | grep Location | wc -l
+cat Exercice1/ann/2016/* | grep Location | wc -l
 echo "Nombre de location en 2017 :"
-cat 2017/* | grep Location | wc -l
+cat Exercice1/ann/2017/* | grep Location | wc -l
 echo "Nombre de location en 2018 :"             
-cat 2018/* | grep Location | wc -l             
+cat Exercice1/ann/2018/* | grep Location | wc -l
